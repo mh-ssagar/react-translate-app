714c4f27cb2b4cd9d2aac59fd7f0665df99f6231
mbh-ssagar <shyam.sagar@mobileheartbeat.com>
2022-06-07 11:58:06 -0400

    Nav to config page=>tab can be added to channel

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import App from './App';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Config from "./Config";
 
-const root = createRoot(document.getElementById('root')); 
-root.render(<App />);
+ReactDOM.render(
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/config" element={<Config />} />
+      </Routes>
+    </Router>,
+  
+    document.getElementById("root")
+  );
+
+// const root = createRoot(document.getElementById('root')); 
+// root.render(<App />);
 
 
